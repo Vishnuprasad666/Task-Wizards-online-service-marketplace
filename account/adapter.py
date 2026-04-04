62a66d5ee5561ef2d8a5c15a661bd7c722da57be
Vishnuprasad M <77232280+Vishnuprasad666@users.noreply.github.com>
2026-04-04 19:28:27 +0530

    implemented Google OAuth 2.0

---
+++ account/adapter.py
@@ -0,0 +1,12 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.shortcuts import resolve_url
+from django.urls import reverse
+
+class RoleSelectionAdapter(DefaultSocialAccountAdapter):
+    def get_login_redirect_url(self, request):
+        path = super().get_login_redirect_url(request)
+        user = request.user
+        if user.is_authenticated and (user.role == "Unassigned"):
+            return reverse("role_selection")
+        return path
