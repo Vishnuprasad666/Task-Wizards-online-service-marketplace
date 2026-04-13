e3930c1b6bcbe77f73d73f66c85ce5a9670e4d2f
Vishnuprasad M <77232280+Vishnuprasad666@users.noreply.github.com>
2026-04-13 21:05:51 +0530

    preparing for deployment

--- Task_Wizards/settings.py
+++ Task_Wizards/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost,.hf.space").split(",")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
@@ -160,7 +160,7 @@ if not DEBUG:
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home"
 
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="http://127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="http://127.0.0.1,https://*.hf.space").split(",")
 
 AUTHENTICATION_BACKENDS=[
     "django.contrib.auth.backends.ModelBackend",
